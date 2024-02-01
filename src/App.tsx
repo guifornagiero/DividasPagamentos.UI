1b5845e7a5913f1e705b349f4f4faefc8f565ca9
guifornagiero <guifornagiero.c@hotmail.com>
2024-01-31 21:06:27 -0300

    feat: adicionar React Router funcional e rotas ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Header from './components/header/header'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <main className="mx-80 my-12 dark:bg-white rounded-xl p-5 light: border-slate-900 border-2">
+      <Header />
+      <Outlet />
+    </main>
+  )
 }
 
 export default App
