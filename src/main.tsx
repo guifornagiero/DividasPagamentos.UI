d6e5d6635acfe232c9bfdaa1c401baf969c2a05b
guifornagiero <guifornagiero.c@hotmail.com>
2024-02-01 22:17:33 -0300

    feat: corrigir estrutura de pastas e apontamentos

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from './App.tsx'
 import '@/globals.css'
 import { ThemeProvider } from '@/components/ui/theme-provider.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import DividasScreen from './components/screens/dividas-screen.tsx'
-import PessoasScreen from './components/screens/pessoas-screen.tsx'
-import EmpresasScreen from './components/screens/empresas-screen.tsx'
+import DividasScreen from './components/screens/dividas/dividas-screen.tsx'
+import PessoasScreen from './components/screens/pessoas/pessoas-screen.tsx'
+import EmpresasScreen from './components/screens/empresas/empresas-screen.tsx'
 
 const router = createBrowserRouter([
   {
